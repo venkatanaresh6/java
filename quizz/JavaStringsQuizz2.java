44ced1684a1d3514288aabf9b9e5e877dbb311a2
venkatanaresh6 <vn525606@gmail.com>
2024-06-11 11:41:56 +0530

    quizz

---
+++ quizz/JavaStringsQuizz2.java
@@ -0,0 +1,8 @@
+public class JavaStringsQuizz2
+{
+    public static void main(String[] args) {
+        String str="  ";
+        System.out.println(str.isBlank());
+        System.out.println(str.isEmpty());
+    }
+}
