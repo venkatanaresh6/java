d9fa6edefd3e60d142b7f6135a8f8f63d671afa1
venkatanaresh6 <vn525606@gmail.com>
2024-06-18 11:44:31 +0530

    18/6

---
+++ quizz/Test3.java
@@ -0,0 +1,6 @@
+public class Test3 {
+    public static void main(String[] args) {
+        String s=new String("5");
+        System.out.println(1+10+s+1+10);
+    }
+}
---
+++ quizz/Test4.java
@@ -0,0 +1,10 @@
+public class Test4 {
+    public static void main(String[] args) {
+        String str=null;
+        switch (str) {
+            case "null":
+                System.out.println("null string");
+                break;
+        }
+    }
+}
